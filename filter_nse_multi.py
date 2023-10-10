45ed4672ac2cd0109e3e8f0119740a1cd3bc1507
Hemanth B <hemanth22hemu@gmail.com>
2023-10-10 22:37:23 +0530

    Create filter_nse_multi.py

---
+++ filter_nse_multi.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_excel("MA-Equities-CM-volume-10-Oct-2023.xlsx", sheet_name="MA-Equities-CM-volume-10-Oct-20")
+#print(df)
+
+filtered_df  = df[(df['%CHNG \n'].astype(float) < 1) & (df['OPEN \n'].astype(float) > 70)]
+print(filtered_df)
+
+filtered_df.to_excel("filtered_excel.xlsx", index=False)
+
+df = pd.read_excel("filtered_excel.xlsx")
+
+selected_columns = ['SYMBOL \n', '%CHNG \n']
+selected_df = df[selected_columns]
+selected_df.to_excel("multi_filtered_excel_cols.xlsx", index=False)
