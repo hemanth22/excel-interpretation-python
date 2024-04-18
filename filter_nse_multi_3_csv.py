7e2fe11069574348d7658fe5e0e670eaa5d3aedc
Hemanth B <hemanth22hemu@gmail.com>
2024-04-18 19:44:36 +0530

    Create filter_nse_multi_3_csv.py

---
+++ filter_nse_multi_3_csv.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv("MA-Equities-CM-volume-28-Oct-2023.csv")
+mask = ~df['SYMBOL \n']
+values_to_filter = ['SUZLON', 'IDEA', 'SOUTHBANK']
+
+filtered_df = df[mask & df['SYMBOL \n'].str.contains('|',.join(values_to_filter), na=False)]
+#print(filtered_df)
+
+selected_columns = filtered_df[['SYMBOL \n', 'OPEN \n', 'HIGH \n', 'LOW \n', '%CHNG \n']]
+
+print(selected_columns)
+
+selected_columns.to_csv('filtered_symbol_data_cols.csv', index=False)
